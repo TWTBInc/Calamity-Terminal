d6a1a26b1a2f6d89f870e136726cd89cbfd16a70
Blowzart <90931827+Blowzart@users.noreply.github.com>
2021-11-12 15:31:56 -0500

    Added all the correct things that work
    They all work now tehee

---
+++ Calamity.sh
@@ -0,0 +1,1 @@
+python Calamity.py
---
+++ INSTALL.sh
@@ -0,0 +1,6 @@
+uname -r
+echo "Wont work unless on the correct version has to be an arch based version"
+sudo pacman -S python
+sudo pacman -S python-pip
+python --version
+./Calamity.sh
