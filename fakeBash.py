133a00bb5a2a59db30491781e9cab7dc270eba45
Ryuzaki Ikari <99619283+RyuzakiIkari@users.noreply.github.com>
2022-02-25 20:07:42 -0500

    updated fakebash to work with windows probably

--- fakeBash.py
+++ fakeBash.py
@@ -1,5 +1,6 @@
 import os
 #This wont get updated unless its broken but it probably won't break 
+#Update, updating fakebash
 
 def clearConsole():
     command = 'clear'
@@ -10,15 +11,15 @@ def clearConsole():
     os.system(command)
 
 clearConsole()
-print("Welcome to Calamity fakeBash, typing terminal will return to Calamity Terminal.Type --help to see the list of available commands.\n")
+print("Welcome to Calamity fakeBash, typing terminal will return to Calamity Terminal. Type --help to see the list of available commands.\n")
 while True:
-	commandInput = input(">> ")
+	commandInput = input("[] ")
 	if commandInput == "clear":
 		clearConsole()
 		print("Welcome to Calamity fakeBash, typing terminal will return to Calamity Terminal.Type --help to see the list of available commands.\n")
 
 	elif commandInput == "terminal":
-		os.system("python Calamity.py")
+		from Calamity import *
 	else:
 		os.system(commandInput)
 		continue
